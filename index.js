8a6dad7b81441c5565a3d0885227c3fd6b8a9875
Delowar Hossain <bscdelowar@yahoo.com>
2023-06-10 23:03:38 +0600

    selected classes deleted api done

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express=require('express')
-const app=express()
-const cors=require('cors')
+const express = require('express')
+const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors')
 require('dotenv').config()
-const port=process.env.PORT || '5000'
+const port = process.env.PORT || '5000'
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req,res)=>{
-    res.send('server running')
+app.get('/', (req, res) => {
+  res.send('server running')
 })
 
 //mongodb start
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ow6kx3p.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,29 +31,52 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const classCollection=client.db('fashionDesign').collection('classes')
-    const instructorCollection=client.db('fashionDesign').collection('instructors')
-    
+    const classCollection = client.db('fashionDesign').collection('classes')
+    const instructorCollection = client.db('fashionDesign').collection('instructors')
+    const selectedClassCollection=client.db('fashionDesign').collection('selectedClass')
     //class collections
-    app.get('/classes', async(req,res)=>{
-        const result=await classCollection.find().toArray();
-        
-        res.send(result)
+    app.get('/classes', async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result)
     })
-    app.get('/classes/topsix', async(req,res)=>{
-      const allClasses=await classCollection.find().sort({'available_seats':-1}).toArray()
-      const topsix=allClasses.slice(0,6)
+
+    app.get('/classes/topsix', async (req, res) => {
+      const allClasses = await classCollection.find().sort({ 'available_seats': -1 }).toArray()
+      const topsix = allClasses.slice(0, 6)
       res.send(topsix)
     })
     //instructor collections
-    app.get('/instructors', async(req,res)=>{
-        const result=await instructorCollection.find().toArray();
-        res.send(result)
+    app.get('/instructors', async (req, res) => {
+      const result = await instructorCollection.find().toArray();
+      res.send(result)
     })
-    app.get('/instructors/topsix', async(req,res)=>{
-      const allInstructors=await instructorCollection.find().sort({'taken_class':-1}).toArray()
-      const topsix=allInstructors.slice(0,6)
+    app.get('/instructors/topsix', async (req, res) => {
+      const allInstructors = await instructorCollection.find().sort({ 'taken_class': -1 }).toArray()
+      const topsix = allInstructors.slice(0, 6)
       res.send(topsix)
+    })
+    //selectedClass collection
+    app.get('/selected/classes', async (req, res) => {
+      const email=req.query.email
+      if(!email){
+        res.send([])
+      }
+      const query={email:email}
+      const result = await selectedClassCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.post('/selected/classes', async (req, res) => {
+      const data = req.body
+      const result = await selectedClassCollection.insertOne(data)
+      res.send(result)
+    })
+
+    app.delete('/selected/classes/:id', async(req,res)=>{
+      const id=req.params.id
+      const query={_id: new ObjectId(id)}
+      const result=await selectedClassCollection.deleteOne(query)
+      res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -68,6 +91,6 @@ run().catch(console.dir);
 //Mongodb end
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-  })
+  console.log(`Server is running on port ${port}`)
+})
 
